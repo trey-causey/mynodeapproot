779f650ad040ad9dfecf816ea02e745c59ca710f
trey-causey <trey.causey@outlook.com>
2020-08-23 18:21:05 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var http = require('http');
+const app = require('express');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen();
