ce468c1773a9c836cc6ac9c8ec756ad2d836990f
trey-causey <trey.causey@outlook.com>
2020-08-23 23:11:34 -0400

    added views and path.join

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 //var http = require('http');
 let express = require('express');
 let chalk = require('chalk');
+let debug = require('debug')('app');
+let morgan = require('morgan');
 const port = process.env.PORT || 3000
 let app = express();
+let path = require('path');
+
+app.use(morgan('combined'));
 
 app.get('/', function (req,res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + ' port: ' + port + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 
 })
 app.listen(port, function () {
-    console.log(`listening on port ${chalk.green(port)}`);
+    debug(`listening on port ${chalk.green(port)}`);
 });
 
 
