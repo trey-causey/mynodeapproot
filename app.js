ba5beba9253e7b61be62923f3df4a758be648b30
trey-causey <trey.causey@outlook.com>
2020-08-23 22:36:04 -0400

    added chalk, port, listen

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var http = require('http');
-var express = require('express');
-var app = express();
+//var http = require('http');
+let express = require('express');
+let chalk = require('chalk');
+const port = process.env.PORT || 3000
+let app = express();
 
 app.get('/', function (req,res) {
     res.send('hello from my app');
 })
-app.listen(3000);
+app.listen(port, function () {
+    console.log(`listening on port ${chalk.green(port)}`);
+});
+
+
+
 /*var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var message = 'It works!\n',
